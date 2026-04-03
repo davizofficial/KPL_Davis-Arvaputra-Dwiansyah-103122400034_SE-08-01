bb9793cdf9014c34f25db4d9cb6447c7db7f6e0c
Davis Arvaputra Dwiansyah <davizarvaofficial13@gmail.com>
2026-04-03 19:11:01 +0700

    init: add index.js into Guided 05_Generics

---
+++ 05_Generics/Guided/index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {String} input
+ * @returns {String}
+ */
+function fizzBuzz(input) {
+  const deretLarik = input.split(" ");
+  /** @type {Array<String>} */
+  let hasil = "";
+
+  for (const d of deretLarik) {
+    const bil = +d;
+    let fz = "";
+
+    if (bil % 3 === 0) {
+      fz += "Fizz";
+    }
+    if (bil % 5 === 0) {
+      fz += "Buzz";
+    }
+    if (fz != "") {
+      hasil += `${fz}`;
+      continue;
+    }
+    hasil += bil;
+  }
+
+  return hasil;
+}
+
+console.log(fizzBuzz("123 35 1 24 15 15 66 25 1 2"));
